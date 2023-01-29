f0d99d4d3c5abf220bb90cfc04c97c26491a83a5
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2023-01-29 01:32:32 -0700

    new commit

--- swagger.js
+++ swagger.js
@@ -5,9 +5,9 @@ const doc = {
         title: 'My API',
         description: 'Contacts API'
     },
-    host: 'localhost:8080',
+    host: 'lesson4-cse-341.onrender.com',
     // host: 'localhost:8080',
-    schemes: ['http']
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
