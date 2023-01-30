985b13c907c23ba342130ae209f26b2ff5ef874e
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2023-01-30 09:38:31 -0700

    Updated host from local host to render

--- swagger.js
+++ swagger.js
@@ -5,9 +5,9 @@ const doc = {
         title: 'My API',
         description: 'Contacts API'
     },
-    //host: 'lesson4-cse-341.onrender.com',
+    host: 'lesson4-cse-341.onrender.com',
     // host: 'localhost:8080',
-    schemes: ['http']
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
